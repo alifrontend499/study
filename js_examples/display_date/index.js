42b228d46d9a5888218a234399e4f78d4b7ec44f
Ali <ali@multichannelcreative.co.uk>
2017-12-14 09:41:29 +0530

    css changess

--- js_examples/display_date/index.js
+++ js_examples/display_date/index.js
@@ -1,27 +1,34 @@
-var hours,minutes,seconds,meridian;
-var d = new Date();
-
-hours = d.getHours();
-
-if(hours > 12){
-	hours = hours - 12;	
+function displayDate() {
+    var hours, minutes, seconds, meridian, secData = "";
+    //DATE
+    var d = new Date();
+    //HOURS
+    hours = d.getHours();
+    if (hours > 12) {
+        hours = hours - 12;
+    }
+    //MINUTES
+    minutes = d.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    //SECONDS
+    seconds = d.getSeconds();
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    secData += seconds;
+    //HOURS
+    if (hours >= 12) {
+        meridian = "PM";
+    } else {
+        meridian = "AM";
+    }
+    //DISPLAY DATE
+    var dateElem = document.getElementById("date");
+    var timeElem = document.getElementById("time");
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
+    dateElem.innerHTML = "Today is <span style='color:red'>" + days[d.getDay()] + "</span><br>";
+    timeElem.innerHTML = "Current time is <span style='color:red'>" + hours + ":" + minutes + ":" + secData + " " + meridian + " </span>";
 }
-
-minutes = d.getMinutes();
-if(minutes < 10){
-	minutes = "0" + minutes;
-}
-
-seconds = d.getSeconds();
-    if(seconds < 10){
-    seconds = "0" + seconds;
-}
-if(hours >= 12){
-	meridian = "AM";
-} else{
-	meridian = "PM";
-}
-
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday"];
-document.write("Today is <span style='color:red'>" + days[d.getDay()] + "</span><br>");
-document.write("Current time is <span style='color:red'>" + hours + meridian + ":" + minutes + ":" + seconds + "</span>");
+setInterval(displayDate, 1000);
