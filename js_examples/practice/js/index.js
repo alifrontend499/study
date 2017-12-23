7d2e83d213d05fefbf32085ea5aba4b8e9bfb5ae
alifrontend499 <aliofficial499@gmail.com>
2017-12-23 20:31:57 +0530

    css changes

--- js_examples/practice/js/index.js
+++ js_examples/practice/js/index.js
@@ -1,36 +1,44 @@
 window.onload = function () {
 // basic styling
   (function () {
-    var bodtyStyle = 'font-family:calibri;margin:0;'
+    var bodtyStyle = 'font-family:calibri;margin:0;font-size: 20px;'
     document.querySelector('body').style.cssText = bodtyStyle
-    var elem = document.getElementById('demo')
+    var elem = document.getElementById('heading')
     elem.style.cssText = 'padding:10px 15px;text-transform:uppercase;text-align:center;'
     elem.innerHTML = '<h1>Js example</h1>'
   })()
+  var output = document.querySelector('.output')
+  output.style.cssText = 'background-color:gray;padding:5px 15px;color: #fff;font-size: 1em;';
 // main js
-  var inputVal = document.querySelector('.name')
-  var getForm = document.querySelector('#get_data')
-  getForm.addEventListener('submit', printData)
-  function printData (e) {
-    e.preventDefault()
-    getForm.innerHTML = 'Hi ' + inputVal.value
-  }
-
-  var person = {
-    'firstName': 'Ali',
-    'lastName': 'Mir',
-    'address': {
-      'city': 'Jalandhar',
-      'state': 'Punjab'
-    },
-    'isFromState': function (stateName) {
-      if (stateName === this.address.state) {
-        console.log('Yes')
-      } else {
-        console.log('No')
+// display date
+  // (function () {
+  //   var today, mm, dd, yy, monthsNames
+  //   today = new Date()
+  //   dd = today.getDate()
+  //   mm = today.getMonth()
+  //   monthsNames = ['January', 'Februry', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  //   yy = today.getFullYear()
+  //   // console.dir(today)
+  //   output.innerHTML += monthsNames[mm] + '-' + dd + '-' + yy + '</br>'
+  //   output.innerHTML += monthsNames[mm] + '/' + dd + '/' + yy
+  // })()
+  // finding area of the triangle
+  // function area (base, height) {
+  //   return `The area of the triangle is : ${1 / 2 * base * height} sq.cm`
+  // }
+  // output.innerHTML += area(6, 4)
+  // rotating the string
+  (function () {
+    function rightAlign (str) {
+      var mainStr = str.split('')
+      var listing = ''
+      // console.log(mainStr.length)
+      for (var i = mainStr.length - 1; i >= 0; i--) {
+        listing += mainStr[i]
       }
-      return 'Hello'
+      return listing
     }
-  }
-  person.isFromState('Punjab')
+    output.innerHTML += rightAlign('w3resource')
+  })()
+  // canvas
 }
